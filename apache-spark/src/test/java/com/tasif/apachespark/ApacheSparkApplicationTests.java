723572dd95f21fd9d502600b91f68188f9431083
tasifmd <tasifmd96@gmail.com>
2019-11-29 16:45:43 +0530

    init

---
+++ apache-spark/src/test/java/com/tasif/apachespark/ApacheSparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasif.apachespark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheSparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
